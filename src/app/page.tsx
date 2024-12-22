6dbefed6beae34e84f3ddf2ea7b62aaa32912090
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-22 22:10:00 +0500

    Add new components AuthorCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
+import AuthorCard from "@/components/AuthorCard";
 export default function Home() {
   return (
    <>
    <Navbar/>
+   <AuthorCard/>
    </>
   );
 }
--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
+import image from "../../public/image.png"
 
 const AuthorCard = () => {
     return (
-        <div>
-            
+        <div className='bg-white shadow-lg rounded-lg p-6 mt-12 '>
+            <div className='flex items-center animation-fadeIn'>
+                <Image
+                    className='w-20 h-20 rounded-full mr-4 object-cover border-2 border-cyan-500'
+                    src={image}
+                    alt='Author image'></Image>
+                <div>
+                    <h3 className='text-xl font-bold'>Muhammad Talha</h3>
+                    <p className='text-slate-500'>Undergraduate BS Computer Science | Web Developer</p>
+                </div>
+            </div>
+
+
+<p>
+Muhammad Talha is an undergraduate in Bs computer Science with a passion for Web developer and sharing Knowledge through  articles
+</p>
         </div>
     );
 }
